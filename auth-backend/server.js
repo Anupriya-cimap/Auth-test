8e7944f358bc52c3a1350404f6fb9aa8fde9bbd6
Anupriya-cimap <anupriyayadavcimap@gmail.com>
2025-02-11 14:55:56 +0530

    commit

--- auth-backend/server.js
+++ auth-backend/server.js
@@ -35,8 +35,7 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+   
   })
   .then(() => console.log("MongoDB connected successfully."))
   .catch((err) => {
